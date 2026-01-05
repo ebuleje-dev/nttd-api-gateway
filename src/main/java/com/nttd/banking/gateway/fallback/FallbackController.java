b9114afdce68b7554844771ff72608c8bc8f74d9
ebuleje-dev <edgard.bulejev@hotmail.com>
2026-01-05 01:56:17 -0500

    config fix

--- src/main/java/com/nttd/banking/gateway/fallback/FallbackController.java
+++ src/main/java/com/nttd/banking/gateway/fallback/FallbackController.java
@@ -1,7 +1,7 @@
 package com.nttd.banking.gateway.fallback;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -11,52 +11,52 @@ import java.util.Map;
 @RestController
 public class FallbackController {
 
-    @GetMapping("/fallback/customers")
+    @RequestMapping("/fallback/customers")
     public Mono<Map<String, Object>> customerFallback() {
         return Mono.just(buildResponse("customer-service"));
     }
 
-    @GetMapping("/fallback/accounts")
+    @RequestMapping("/fallback/accounts")
     public Mono<Map<String, Object>> accountFallback() {
         return Mono.just(buildResponse("account-service"));
     }
 
-    @GetMapping("/fallback/credits")
+    @RequestMapping("/fallback/credits")
     public Mono<Map<String, Object>> creditFallback() {
         return Mono.just(buildResponse("credit-service"));
     }
 
-    @GetMapping("/fallback/credit-cards")
+    @RequestMapping("/fallback/credit-cards")
     public Mono<Map<String, Object>> creditCardFallback() {
         return Mono.just(buildResponse("credit-card-service"));
     }
 
-    @GetMapping("/fallback/debit-cards")
+    @RequestMapping("/fallback/debit-cards")
     public Mono<Map<String, Object>> debitCardFallback() {
         return Mono.just(buildResponse("debit-card-service"));
     }
 
-    @GetMapping("/fallback/transactions")
+    @RequestMapping("/fallback/transactions")
     public Mono<Map<String, Object>> transactionFallback() {
         return Mono.just(buildResponse("transaction-service"));
     }
 
-    @GetMapping("/fallback/reports")
+    @RequestMapping("/fallback/reports")
     public Mono<Map<String, Object>> reportFallback() {
         return Mono.just(buildResponse("report-service"));
     }
 
-    @GetMapping("/fallback/yanki")
+    @RequestMapping("/fallback/yanki")
     public Mono<Map<String, Object>> yankiFallback() {
         return Mono.just(buildResponse("yanki-service"));
     }
 
-    @GetMapping("/fallback/bootcoin")
+    @RequestMapping("/fallback/bootcoin")
     public Mono<Map<String, Object>> bootcoinFallback() {
         return Mono.just(buildResponse("bootcoin-service"));
     }
 
-    @GetMapping("/fallback/auth")
+    @RequestMapping("/fallback/auth")
     public Mono<Map<String, Object>> authFallback() {
         return Mono.just(buildResponse("auth-service"));
     }
